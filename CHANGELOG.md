7a570096e23c59f8375f864a480a17cb72014e54
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-14 15:19:25 -0700

    Changes to change log and app
    Re-writing some components and making changes to the log.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
+
 * 2020.09.14 master
   - Initial push to github and setup. 
+  - Includes some changes to the UI
--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import NavigationBar from './components/navigationBar';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <NavigationBar />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
