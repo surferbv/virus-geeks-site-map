3d029086c77305ed47b5f2692633098c84b8bcc0
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-14 17:42:57 -0700

    Removed header This has styling that I don't really need at the moement.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Types from './components/materialTypograph'
+import Types from './components/materialTypograph';
 import Container from '@material-ui/core/Container';
-
+import SimplePaper from './components/simplePaper';
+import Paper from '@material-ui/core/Paper';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Container maxWidth="sm">
-          <h1>HI BOB!</h1>
-          <Types/>
-        </Container>
-      </header>
+      <Container >
+        <SimplePaper/>
+      </Container>
     </div>
   );
 }
