e9ebe08d61f2ac26a080a6813b9cc1b393998c07
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-14 17:05:11 -0700

    Themeing
    Seeing if the material themes work

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-import NavigationBar from './components/navigationBar';
+import Types from './components/materialTypograph'
+import Container from '@material-ui/core/Container';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <NavigationBar />
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container maxWidth="sm">
+          <h1>HI BOB!</h1>
+          <Types/>
+        </Container>
       </header>
     </div>
   );
