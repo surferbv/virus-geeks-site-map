8ecec734cbe3d6791093b2f78942e9e598f2bde9
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-16 14:49:57 -0700

    Updates to App
    Remove imports and add new components.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-import Types from './components/materialTypograph';
-import Container from '@material-ui/core/Container';
-import SimplePaper from './components/simplePaper';
-import Paper from '@material-ui/core/Paper';
+import MapSection from './components/mapSection';
+import BannerSection from './components/bannerSection';
+import TitleSection from './components/titleSection';
 
 function App() {
   return (
     <div className="App">
-      <Container >
-        <SimplePaper/>
-      </Container>
+      
+      <BannerSection/>
+      <TitleSection/>
+      <MapSection/>
     </div>
   );
 }
