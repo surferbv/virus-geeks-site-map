f12694f21f2da303d6ec9550d443746437538069
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-16 17:29:06 -0700

    Remove white space

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import MapSection from './components/mapSection';
 import BannerSection from './components/bannerSection';
 import TitleSection from './components/titleSection';
 
+
 function App() {
   return (
     <div className="App">
-      
       <BannerSection/>
       <TitleSection/>
       <MapSection/>
