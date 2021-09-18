157783ce15ea4af6b4b534c583c555dfedfe9dbb
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-17 17:27:30 -0700

    White space

--- src/components/mapCard.js
+++ src/components/mapCard.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import MapBox from './mapBox';
 
-
-
 export default function MapCard(){
     return(
-        <Card sx={{ minWidth: 300, height: 800 }} elevation={14} >
+        <Card sx={{ minWidth: 300, height: 800}} elevation={14} >
             <MapBox/>
         </Card>
     )
