f32a7dc55c0e8f3c21d1d66b933152cc5303243c
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-23 15:41:40 -0700

    Import Turf

--- src/components/mapBox.js
+++ src/components/mapBox.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import Alert from "@mui/material/Alert";
+import * as turf from '@turf/turf';
 
 
 mapboxgl.accessToken =
