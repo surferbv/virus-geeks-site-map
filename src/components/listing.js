5e2c44132aabb71be2a13266cb0af189c5c27804
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-09-29 13:55:07 -0700

    Props
    Passed a prop to the listing element

--- src/components/listing.js
+++ src/components/listing.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Card from '@mui/material/Card';
 
+// a listing item to go into the side bar component 
 export default function Listing(props){
-
-    // note that listing is a card div 
     return(
-        <Card sx={{ borderRadius: 5 }} elevation={7}>
+        <Card sx={{ borderRadius: 5 }} elevation={7} id={props.myID} >
             <p> Site information goes here </p>
         </Card>
     )
