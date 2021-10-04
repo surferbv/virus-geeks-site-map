53f5f4d1095ab97b786668dee109e2bade3cf463
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-04 13:58:20 -0700

    Change banner
    added appbar.

--- src/components/bannerSection.js
+++ src/components/bannerSection.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
 
-export default function BannerSection(){
-    return(
-        <Container maxWidth='xl'>
-
-            <Grid container
-                direction="row"
-                justifyContent="flex-start"
-                alignItems="center"
-                spacing={1}
+export default function BannerSection() {
+  return (
+    <Box sx={{ flexGrow: 1, pb: 15 }}>
+      <AppBar position="fixed" sx={{ pl: 1, pr: 4, background: "white" }}>
+        <Container>
+          <Toolbar>
+            <img src="/vg_logo.png" alt="virus geeks" width="40" height="40" />
+            <Typography
+              variant="h5"
+              component="div"
+              sx={{ flexGrow: 1, color: "black", pl: 2 }}
             >
-                <Grid item>
-                    <img src="/vg_logo.png" alt="virus geeks" width="80" height="80"/>
-                </Grid>
-                <Grid item>
-                    <Typography variant="h5" noWrap>
-                        Virus Geeks
-                    </Typography>
-                </Grid>
-
-            </Grid>
+              Virus Geeks
+            </Typography>
+            <Button>Login</Button>
+          </Toolbar>
         </Container>
-    )
+      </AppBar>
+    </Box>
+  );
 }
