9cfed161970fd171dd37cc6d0883d3801bbd7e1f
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-08 10:10:29 -0700

    Elevation zero

--- src/components/mapCard.js
+++ src/components/mapCard.js
@@ -4,7 +4,7 @@ import MapBox from "./mapBox";
 
 export default function MapCard() {
   return (
-        <Card sx={{ borderRadius: 5 }} elevation={14}>
+        <Card sx={{ borderRadius: 0 }} elevation={14}>
             <MapBox />
         </Card>
   );
