1920df51cc10da4d1dcddc58acf3872796e430e0
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-08 11:41:39 -0700

    Disable drag on map
    See what happens on mobile.

--- src/components/mapSection.js
+++ src/components/mapSection.js
@@ -76,6 +76,8 @@ export default function MapBox() {
           zoom: zoom
         }); 
 
+        map.current.dragPan.disable();
+
         // will fire once the map has loaded
         map.current.on('load', () =>{
           
