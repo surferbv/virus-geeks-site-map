2cc73ac91804ac1d5f96ee1063e1ef3903b2cfda
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-11 10:33:23 -0700

    Make title responsive
    Applying theme provider for responsive fonts.

--- src/components/titleSection.js
+++ src/components/titleSection.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@mui/material/styles";
 
 export default function titleSection() {
+  let theme = createTheme();
+  theme = responsiveFontSizes(theme);
+
   return (
-      
     <Container>
-      <Typography variant="h2" textAlign="center" >
-        Testing Sites
-      </Typography>
+      <ThemeProvider theme={theme}>
+        <Typography variant="h2" textAlign="center">
+          Testing Sites
+        </Typography>
+      </ThemeProvider>
     </Container>
   );
 }
