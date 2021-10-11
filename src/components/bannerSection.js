0a5ad02007632aacf28f0f993a914f8fd96b5f54
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-11 10:34:25 -0700

    Apply responsive theme
    Also remove some padding.

--- src/components/bannerSection.js
+++ src/components/bannerSection.js
@@ -6,22 +6,34 @@ import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+
 
 export default function BannerSection() {
+  let theme = createTheme();
+  theme = responsiveFontSizes(theme);
+
+
   return (
     <Box sx={{ flexGrow: 1, pb: 8 }}>
-      <AppBar position="fixed" sx={{ pl: 1, pr: 4, background: "white" }}>
+      <AppBar position="fixed" sx={{background: "white" }}>
         <Container>
           <Toolbar>
             <img src="/vg_logo.png" alt="virus geeks" width="40" height="40" />
-            <Typography
-              variant="h5"
-              component="div"
-              sx={{ flexGrow: 1, color: "black", pl: 2 }}
-            >
-              Virus Geeks
-            </Typography>
+            <ThemeProvider theme={theme}> 
+              <Typography
+                variant="h5"
+                component="div"
+                sx={{ flexGrow: 1, color: "black", pl: 1 }}
+              >
+                Virus Geeks
+              </Typography>
             <Button href="https://my.virusgeeks.com/login" >Login</Button>
+            </ThemeProvider>
           </Toolbar>
         </Container>
       </AppBar>
