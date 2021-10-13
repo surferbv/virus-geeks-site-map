0bd543fa33750291ceb44312b7d714660d426467
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-12 17:09:05 -0700

    Removed un-used import

--- src/components/bannerSection.js
+++ src/components/bannerSection.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
