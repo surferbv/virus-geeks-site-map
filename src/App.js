4824cd41b28f44283a6e522dff39bc2d50286300
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-13 16:30:25 -0700

    Adding rest of the sections

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import MapSection from './components/mapSection';
 import MapTitleSection from './components/mapTitleSection';
 import GetTestedSection from './components/getTestedSection';
+import GetResultsSection from './components/getResultsSection';
+import GetTestedQuestionSection from './components/getTestedQuestionSection';
 import AppBarSection from './components/appBarSection';
+import AppFooterSection from './components/appFooterSection';
 
 
 function App() {
@@ -9,8 +12,15 @@ function App() {
     <div className="App">
       <AppBarSection/>
       <GetTestedSection/>
-      <MapTitleSection/>
+      <GetResultsSection/>
+
+      <MapTitleSection/> {/* could move this into mapsection */}
       <MapSection/>
+
+
+      <GetTestedQuestionSection/>
+      <AppFooterSection/>
+
     </div>
   );
 }
