3eba9d7fac4323ec1f799cdb0c071a6ef2184b6a
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-13 11:22:06 -0700

    Get tested init section

---
+++ src/components/getTestedSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import { render } from "react-dom";
+
+export default function GetTestedSection(){
+    return(
+        <Container>
+        
+        
+
+
+        </Container>
+    );
+}
