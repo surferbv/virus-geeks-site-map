0b286d261e6dfd16e10b6de4f7c377719951efee
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-12 20:25:57 -0700

    Hide appbar on scroll

--- src/components/bannerSection.js
+++ src/components/bannerSection.js
@@ -10,32 +10,41 @@ import {
   ThemeProvider,
   responsiveFontSizes,
 } from "@mui/material/styles";
-
+import clsx from "clsx";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from '@mui/material/Slide';
 
 export default function BannerSection() {
+  const trigger = useScrollTrigger();
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
 
-
   return (
     <Box sx={{ flexGrow: 1, pb: 8 }}>
-      <AppBar position="fixed" sx={{background: "white" }}>
-        <Container>
-          <Toolbar>
-            <img src="/vg_logo.png" alt="virus geeks" width="40" height="40" />
-            <ThemeProvider theme={theme}> 
-              <Typography
-                variant="h5"
-                component="div"
-                sx={{ flexGrow: 1, color: "black", pl: 1 }}
-              >
-                Virus Geeks
-              </Typography>
-            <Button href="https://my.virusgeeks.com/login" >Login</Button>
-            </ThemeProvider>
-          </Toolbar>
-        </Container>
-      </AppBar>
+      <Slide in={!trigger}>
+        <AppBar position="fixed" sx={{ background: "white" }}>
+          <Container>
+            <Toolbar>
+              <img
+                src="/vg_logo.png"
+                alt="virus geeks"
+                width="40"
+                height="40"
+              />
+              <ThemeProvider theme={theme}>
+                <Typography
+                  variant="h5"
+                  component="div"
+                  sx={{ flexGrow: 1, color: "black", pl: 1 }}
+                >
+                  Virus Geeks
+                </Typography>
+                <Button href="https://my.virusgeeks.com/login">Login</Button>
+              </ThemeProvider>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </Slide>
     </Box>
   );
 }
