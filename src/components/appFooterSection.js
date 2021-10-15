fd68a1a290a436eceb86caab928d1e9fa69db675
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2021-10-14 17:39:24 -0700

    Adjusting the size of the text
    Based on screen size

--- src/components/appFooterSection.js
+++ src/components/appFooterSection.js
@@ -26,5 +26,52 @@ export default function AppFooterSection() {
         </Box>
       </Container>
     </React.Fragment>
+    <Container maxWidth="xl" sx={{ pt:12, pb: 4, bgcolor: 'secondary.main.light' }} >
+        <Divider variant="middle"/>
+      <Grid container rowSpacing={2} columnSpacing={2} direction="row" justifyContent="center" alignItems="center">
+        <Grid item xs={12} sm >
+          <InnerItem>
+            <Link href="#" underline="always" variant="button" sx={{ fontSize: {sm: 10, md: 16} }} gutterBottom>
+              Virus Geeks
+            </Link>
+          </InnerItem>
+        </Grid>
+        <Grid item xs={12} sm >
+          <InnerItem>
+            <Link href="#" underline="always" variant="button" sx={{ fontSize: {sm: 10, md: 16} }} gutterBottom>
+              Contact us
+            </Link>
+          </InnerItem>
+        </Grid>
+        <Grid item xs={12} sm >
+          <InnerItem>
+            <Link href="#" underline="always" variant="button" sx={{ fontSize: {sm: 10, md: 16} }} gutterBottom>
+              Terms of service
+            </Link>
+          </InnerItem>
+        </Grid>
+        <Grid item xs={12} sm >
+          <InnerItem>
+            <Link href="#" underline="always" variant="button" sx={{ fontSize: {sm: 10, md: 16} }} gutterBottom>
+              Notice of privacy practices
+            </Link>
+          </InnerItem>
+        </Grid>
+        <Grid item xs={12} sm >
+          <InnerItem>
+            <Link href="#" underline="always" variant="button" sx={{ fontSize: {sm: 10, md: 16} }} gutterBottom>
+              Covide-19 informed consent
+            </Link>
+          </InnerItem>
+        </Grid>
+      </Grid>
+      <InnerItem>
+        <ThemeProvider theme={responsiveFontTheme}>
+          <Typography variant="subtitle1">
+            © 2021 Copyright Virus Geeks Inc.
+          </Typography>
+        </ThemeProvider>
+      </InnerItem>
+    </Container>
   );
 }
